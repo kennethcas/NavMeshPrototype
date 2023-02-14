df93a1655f5292c475711829d0215c8247ed7543
kenneth <kc4338@nyu.edu>
2023-02-14 13:51:35 -0500

    Bugs fixed, enemies added, UI added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Ken.CharacterPack
+{
+    public class Enemy : MonoBehaviour
+    {
+        NavMeshAgent nav;
+
+        [SerializeField]
+        Transform body;
+        [SerializeField]
+        Animator anim;
+
+        bool isDying = false;
+
+        bool isWalking = false;
+
+        float moveTimer = 0;
+        float moveTimerTotal = 0;
+        void Start()
+        {
+            nav = GetComponent<NavMeshAgent>();
+            //ResetMoveTimer();
+            nav.SetDestination(transform.position + new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)));
+            moveTimerTotal = Random.Range(1.1f, 3.3f);
+        }
+
+        void ResetMoveTimer()
+        {
+            moveTimer = 0;
+            moveTimerTotal = Random.Range(1.1f, 3.3f);
+            //nav.SetDestination(transform.position + new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)));
+
+        }
+
+        void Update()
+        {
+
+            if (!isDying)
+            {
+                if (moveTimer > moveTimerTotal)
+                {
+                    //ResetMoveTimer();
+                    nav.isStopped = false;
+                    //nav.SetDestination(transform.position + new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)));
+                    nav.speed = 3;
+
+                }
+                else if (moveTimer < moveTimerTotal)
+                {
+                    ResetMoveTimer();
+                    nav.SetDestination(transform.position + new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)));
+                }
+            }
+            else if (isDying)
+            {
+                //PLAY DYING ANIMATION
+                StartCoroutine(DespawnEnemyCoroutine());
+                anim.SetTrigger("Dying");
+            }
+
+            if (nav.velocity.magnitude < 0.1f)
+            {
+                anim.SetBool("isWalking", false);
+            }
+            else
+            {
+                anim.SetBool("isWalking", true);
+                //anim.SetFloat("Velocity", nav.velocity.magnitude);
+            }
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                Debug.Log("chicken colliding with player");
+                isDying = true;
+            }
+        }
+
+        IEnumerator DespawnEnemyCoroutine()
+        {
+            yield return new WaitForSeconds(2);
+            Destroy(gameObject);
+            
+
+        }
+    }
+
+}
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    //public Camera cam;
+    //public NavMeshAgent agent;
+
+    public float movementSpeed = 3;
+    public float jumpForce = 300;
+    public float timeBeforeNextJump = 1.2f;
+    private float canJump = 0f;
+
+    private float canAttack = 0f;
+    public float timeBeforeNextAttack = 1.2f;
+
+    Animator anim;
+    Rigidbody rb;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //MovePlayer();
+        ControlPlayer();
+    }
+
+    void ControlPlayer()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+            anim.SetInteger("Walk", 1);
+
+            rb.velocity = movement;
+            //transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+        }
+        else {
+            anim.SetInteger("Walk", 0);
+        }
+
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+
+        /*if (Input.GetButtonDown("Jump") && Time.time > canJump)
+        {
+                rb.AddForce(0, jumpForce, 0);
+                canJump = Time.time + timeBeforeNextJump;
+                anim.SetTrigger("jump");
+        }*/
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > canAttack)
+        {
+            canAttack = Time.time + timeBeforeNextAttack;
+            anim.SetTrigger("attack");
+        }
+
+
+    }
+
+    /*void MovePlayer()
+    {
+        if (Input.GetMouseButtonDown(0)) //if left mouse clicked
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                //MOVE
+                agent.SetDestination(hit.point);
+
+            }
+        }
+    }*/
+}
